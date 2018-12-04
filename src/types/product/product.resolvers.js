ab1e3b9accd4d2b68804b6a538bd5a6c30f46992
Scott Moss <willscottmoss@gmail.com>
2018-12-03 23:17:37 -0600

    chore

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -9,54 +9,10 @@ const productsTypeMatcher = {
   DRONE: 'Drone'
 }
 
-/** product */
-const product = (_, args, ctx) => {
-  return Product.findById(args.id)
-    .lean()
-    .exec()
-}
-
-const newProduct = (_, args, ctx) => {
-  // use this fake ID for createdBy for now until we talk auth
-  const createdBy = mongoose.Types.ObjectId()
-  return Product.create({ ...args.input, createdBy })
-}
-
-const products = (_, args, ctx) => {
-  return Product.find({})
-    .lean()
-    .exec()
-}
-
-const updateProduct = (_, args, ctx) => {
-  const update = args.input
-  return Product.findByIdAndUpdate(args.id, update, { new: true })
-    .lean()
-    .exec()
-}
-
-const removeProduct = (_, args, ctx) => {
-  return Product.findByIdAndRemove(args.id)
-    .lean()
-    .exec()
-}
-
 export default {
-  Query: {
-    products,
-    product
-  },
-  Mutation: {
-    newProduct,
-    updateProduct,
-    removeProduct
-  },
+  Query: {},
+  Mutation: {},
   Product: {
-    __resolveType(product) {},
-    createdBy(product) {
-      return User.findById(product.createdBy)
-        .lean()
-        .exec()
-    }
+    __resolveType(product) {}
   }
 }
