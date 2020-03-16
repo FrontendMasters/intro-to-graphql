7c079807385615fe3a62a2b68f0be7b8a98f6f88
Brijesh Panara <brijesh.panara@sprinklr.com>
2020-03-16 13:09:37 +0530

    Add Cat type and myCat query

--- src/server.js
+++ src/server.js
@@ -10,7 +10,17 @@ import user from './types/user/user.resolvers'
 const types = ['product', 'coupon', 'user']
 
 export const start = async () => {
-  const rootSchema = `
+  const rootSchema = `    
+    type Cat {
+      name: String,
+      age: Int,
+      bestFriend: Cat
+    }
+
+    type Query {
+      myCat: Cat
+    }
+
     schema {
       query: Query
     }
@@ -19,7 +29,21 @@ export const start = async () => {
 
   const server = new ApolloServer({
     typeDefs: [rootSchema],
-    resolvers: {},
+    resolvers: { 
+      Query: { 
+        myCat() { 
+          return {
+            name : "Tom", 
+            age: 2,
+            bestFriend: {
+              name: "Tommy",
+              age: 1,
+              bestFriend: {}
+            }
+          } 
+        }
+      } 
+    },
     context({ req }) {
       // use the authenticate function from utils to auth req, its Async!
       return { user: null }
